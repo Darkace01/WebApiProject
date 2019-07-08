9a984b900c9bcabd56e3c2649a9fd40d01e1f2a8
Darkace01 <33027659+Darkace01@users.noreply.github.com>
2019-07-08 12:54:58 +0000

    Add routing method and versioning

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -11,18 +11,21 @@ namespace ECommerceStoreWebApi.Controllers
 {
     /// <summary>
     /// Product controller
+    /// NOTE: This is version One (1
     /// </summary>
+    [RoutePrefix("api/v2/Product")]
     public class ProductsController : ApiController
     {
         /// <summary>
-        /// Gets some very important data from the server.
+        /// Gets The List of product from the database.
         /// </summary>
+        [Route("allProduct")]
         public HttpResponseMessage Get()
         {
             using (ApplicationDbEntitties entities = new ApplicationDbEntitties())
             {
-                    return Request.CreateResponse(HttpStatusCode.OK,
-                                entities.Products.ToList());
+                return Request.CreateResponse(HttpStatusCode.OK,
+                            entities.Products.ToList());
             }
         }
 
@@ -30,6 +33,7 @@ namespace ECommerceStoreWebApi.Controllers
         /// Looks up product data by ID.
         /// </summary>
         /// <param name="id">The ID of the data.</param>
+        [Route("product/{id}")]
         public Product Get(int id)
         {
             using (ApplicationDbEntitties entities = new ApplicationDbEntitties())
@@ -41,20 +45,32 @@ namespace ECommerceStoreWebApi.Controllers
         /// <summary>
         /// Adds product to the database
         /// </summary>
+        [Route("save")]
         public HttpResponseMessage Post(Product product)
         {
             try
             {
                 using (ApplicationDbEntitties entities = new ApplicationDbEntitties())
                 {
-                    var newproduct = new Product() { ItemName = product.ItemName, ItemPrice = product.ItemPrice, Description = product.Description, Quantity = product.Quantity };
-                    entities.Products.Add(newproduct);
-                    entities.SaveChanges();
-                    var message = Request.CreateResponse(HttpStatusCode.Created, product);
-                    message.Headers.Location = new Uri(Request.RequestUri +
-                        product.ID.ToString());
+                    if (product.ItemName.StartsWith("!,@,#,$,%,^,&,*,(,)._.+,=,-,],[,',;,/,.,>,<,?,|,`.~"))
+                    {
+                        throw new HttpResponseException(HttpStatusCode.NotModified);
+                    }
+                    else if (product.ItemPrice < 0)
+                    {
+                        throw new HttpResponseException(HttpStatusCode.NotModified);
+                    }
+                    else
+                    {
+                        var newproduct = new Product() { ItemName = product.ItemName, ItemPrice = product.ItemPrice, Description = product.Description, Quantity = product.Quantity };
+                        entities.Products.Add(newproduct);
+                        entities.SaveChanges();
+                        var message = Request.CreateResponse(HttpStatusCode.Created, product);
+                        message.Headers.Location = new Uri(Request.RequestUri +
+                            product.ID.ToString());
 
-                    return message;
+                        return message;
+                    }
                 }
             }
             catch (Exception ex)
@@ -68,24 +84,40 @@ namespace ECommerceStoreWebApi.Controllers
         /// </summary>
         /// <param name="id">The ID of the data.</param>
         /// <param name="product">The product to be added</param>
+        [Route("update")]
         public void Put(int id, [FromBody]Product product)
         {
             using (ApplicationDbEntitties entities = new ApplicationDbEntitties())
             {
-                var entity = entities.Products.FirstOrDefault(e => e.ID == id);
+                if (product.ItemName.StartsWith("!,@,#,$,%,^,&,*,(,)._.+,=,-,],[,',;,/,.,>,<,?,|,`.~"))
+                {
+                    throw new HttpResponseException(HttpStatusCode.NotModified);
+                }
+                else if (product.ItemPrice < 0)
+                {
+                    throw new HttpResponseException(HttpStatusCode.NotModified);
+                }
+                else
+                {
 
-                entity.ItemName = product.ItemName;
-                entity.ItemPrice = product.ItemPrice;
-                entity.Quantity = product.Quantity;
-                entity.Description = product.Description;
+                    var entity = entities.Products.FirstOrDefault(e => e.ID == id);
 
-                entities.SaveChanges();
+                    entity.ItemName = product.ItemName;
+                    entity.ItemPrice = product.ItemPrice;
+                    entity.Quantity = product.Quantity;
+                    entity.Description = product.Description;
+
+                    entities.SaveChanges();
+                }
             }
+
         }
+      
         /// <summary>
         /// Deleted product by the given ID.
         /// </summary>
         /// <param name="id">The ID of the data.</param>
+        [Route("delete")]
         public HttpResponseMessage Delete(int id)
         {
             try { 
--- Models/Product.cs
+++ Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,13 @@ namespace ECommerceStoreWebApi.Models
     public class Product
     {
         public int ID { get; set; }
+        [Required(ErrorMessage = "Item name is required")]  
         public string ItemName { get; set; }
+        [Required]
         public double ItemPrice { get; set; }
+        [Required]
         public int Quantity { get; set; }
+        [Required]
         public string Description { get; set; }
     }
 }
